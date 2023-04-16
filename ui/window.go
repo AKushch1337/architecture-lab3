17a72659f981195bb490fe3b47f43fb6dcccadb9
Artem Kushch <artemkusch2@gmail.com>
2023-04-16 14:31:10 +0300

    Change the default size of the window.
    Change the background color to white. Draw a yellow cross in the middle of the window

--- ui/window.go
+++ ui/window.go
@@ -47,7 +47,9 @@ func (pw *Visualizer) run(s screen.Screen) {
 	}
 
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -131,9 +133,11 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	pw.w.Fill(image.Rect(250, 300, 550, 400), color.RGBA{R: 219, G: 208, B: 48, A: 1}, draw.Src)
+	pw.w.Fill(image.Rect(350, 200, 450, 500), color.RGBA{R: 219, G: 208, B: 48, A: 1}, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
