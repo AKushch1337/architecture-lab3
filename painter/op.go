724e7245dfe209cdb17d6e9bd440e1d15358e9da
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2023-04-18 13:56:18 +0300

    all types of operations were added

--- painter/op.go
+++ painter/op.go
@@ -1,6 +1,8 @@
 package painter
 
 import (
+	"golang.org/x/image/draw"
+	"image"
 	"image/color"
 
 	"golang.org/x/exp/shiny/screen"
@@ -45,4 +47,42 @@ func WhiteFill(t screen.Texture) {
 // GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+}
+
+type BgRectOp struct {
+	X1, Y1, X2, Y2 int
+}
+
+func (op *BgRectOp) Do(t screen.Texture) bool {
+	t.Fill(image.Rect(op.X1, op.Y1, op.X2, op.Y2), color.Black, screen.Src)
+	return false
+}
+
+type FigureOp struct {
+	X, Y int
+	C    color.RGBA
+}
+
+func (op *FigureOp) Do(t screen.Texture) bool {
+	t.Fill(image.Rect(op.X-200, op.Y+100, op.X+200, op.Y-100), op.C, draw.Src)
+	t.Fill(image.Rect(op.X-100, op.Y-200, op.X+100, op.Y+200), op.C, draw.Src)
+	return false
+}
+
+type MoveOp struct {
+	X, Y    int
+	Figures []FigureOp
+}
+
+func (op *MoveOp) Do(t screen.Texture) bool {
+	for i := range op.Figures {
+		op.Figures[i].X = op.X
+		op.Figures[i].Y = op.Y
+		op.Figures[i].Do(t)
+	}
+	return false
+}
+
+func Reset(t screen.Texture) {
+	t.Fill(t.Bounds(), color.Black, draw.Src)
 }
