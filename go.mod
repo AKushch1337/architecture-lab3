296437690ff40f920a6906680a63e1b2b9fb2dc8
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2023-04-18 17:26:36 +0300

    parser test was added

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/AKushch1337/architecture-lab3
 go 1.20
 
 require (
+	github.com/stretchr/testify v1.8.2
 	golang.org/x/exp/shiny v0.0.0-20230321023759-10a507213a29
 	golang.org/x/image v0.7.0
 	golang.org/x/mobile v0.0.0-20201217150744-e6ae53a27f4f
@@ -10,7 +11,10 @@ require (
 
 require (
 	dmitri.shuralyov.com/gpu/mtl v0.0.0-20221208032759-85de2813cf6b // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200222043503-6f7a984d4dc4 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/parser_test.go
@@ -0,0 +1,81 @@
+package test
+
+import (
+	"github.com/AKushch1337/architecture-lab3/painter"
+	"github.com/AKushch1337/architecture-lab3/painter/lang"
+	"image/color"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_parse_func(t *testing.T) {
+	tests := []struct {
+		command string
+		op      painter.Operation
+	}{
+		{
+			command: "white",
+			op:      painter.OperationFunc(painter.WhiteFill),
+		},
+		{
+			command: "green",
+			op:      painter.OperationFunc(painter.GreenFill),
+		},
+		{
+			command: "reset",
+			op:      painter.OperationFunc(painter.Reset),
+		},
+	}
+	parser := &lang.Parser{}
+	for _, tt := range tests {
+		t.Run(tt.command, func(t *testing.T) {
+			op, _ := parser.Parse(strings.NewReader(tt.command))
+			assert.Equal(t, reflect.TypeOf(tt.op), reflect.TypeOf(op[0]))
+		})
+	}
+}
+
+func Test_parse_struct(t *testing.T) {
+	tests := []struct {
+		command string
+		op      painter.Operation
+	}{
+		{
+			command: "bgrect 0 0 100 100",
+			op:      &painter.BgRectOp{X1: 0, Y1: 0, X2: 100, Y2: 100},
+		},
+		{
+			command: "figure 200 200",
+			op:      &painter.FigureOp{X: 200, Y: 200, C: color.RGBA{R: 219, G: 208, B: 48, A: 1}},
+		},
+		{
+			command: "move 100 100",
+			op:      &painter.MoveOp{X: 100, Y: 100},
+		},
+		{
+			command: "update",
+			op:      painter.UpdateOp,
+		},
+		{
+			command: "invalidcommand",
+			op:      nil,
+		},
+	}
+	parser := &lang.Parser{}
+	for _, tt := range tests {
+		t.Run(tt.command, func(t *testing.T) {
+			op, err := parser.Parse(strings.NewReader(tt.command))
+			if err != nil {
+				assert.Nil(t, tt.op)
+			} else {
+				assert.Equal(t, reflect.TypeOf(tt.op), reflect.TypeOf(op[0]))
+				if tt.op != nil {
+					assert.Equal(t, tt.op, op[0])
+				}
+			}
+		})
+	}
+}
